f01f1e43485458d7449372a9931ab8f4ab3369ed
Christine Baik <christuaik@gmail.com>
2023-08-06 15:45:20 -0700

    complete

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,11 @@
-# your code goes here!
+def countdown(num):
+    while num >= 1:
+        print(f'{num} SECOND(S)!')
+        num -= 1
+    print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(num):
+    while num >= 1:
+        print(f'{num} SECOND(S)!')
+        num -= 1
+    print("HAPPY NEW YEAR!")
